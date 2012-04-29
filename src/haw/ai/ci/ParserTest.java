058feef6658c22b8b2bfbc03575e42c5b26f4fee
Till Theis <till_theis2@yahoo.de>
2012-04-29 20:33:21 +0200

    add first test

---
+++ src/haw/ai/ci/ParserTest.java
@@ -0,0 +1,24 @@
+package haw.ai.ci;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.junit.*;
+
+public class ParserTest {
+    private Parser createParser(String code) {
+        Scanner scanner = new Scanner(new StringReader(code));
+        Parser parser = new Parser(scanner, "test");
+        parser.insymbol();
+        return parser;
+    }
+    
+    @Test
+    public void testProgram() throws IOException {
+        AbstractNode actual = createParser("123\n32 1").program();
+        AbstractNode expected = new IntNode(1);
+        assertEquals(expected, actual);
+    }
+}

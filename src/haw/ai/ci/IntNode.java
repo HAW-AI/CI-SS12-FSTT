83e97ed41dda848e47c413dfaf397538a4615b3a
Till Theis <till_theis2@yahoo.de>
2012-04-29 20:32:54 +0200

    add equals() and hashCode()

--- src/haw/ai/ci/IntNode.java
+++ src/haw/ai/ci/IntNode.java
@@ -11,8 +11,33 @@ public class IntNode extends AbstractNode {
         intVal = val;
     }
 
+
+    @Override
     protected String toString(int indent) {
         return indentString(indent, "IntNode(" + intVal + ")");
+    }
+    
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + intVal;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        IntNode other = (IntNode) obj;
+        if (intVal != other.intVal)
+            return false;
+        return true;
     }
 
 }

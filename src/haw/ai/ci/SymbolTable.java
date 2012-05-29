4da3269b08652e0e82e606b3cb83d85dde70d0d5
Sebastian <sebastian@bla>
2012-05-29 15:23:52 +0200

    symbolTable

--- src/haw/ai/ci/SymbolTable.java
+++ src/haw/ai/ci/SymbolTable.java
@@ -1,20 +1,73 @@
 package haw.ai.ci;
 
-/**
- * The symbol table.
- * 
- * Declarations (used in Module and Procedure) need the parent table to look
- * up typedefs ("TYPE t = ARRAY[6] OF INTEGER"). Thus, the symbol table must
- * be built incrementally.
- * 
- * Required constructors:
- * SymbolTable()
- * SymbolTable(SymbolTable parent)
- */
-public interface SymbolTable {
-    void declare(String ident, Descriptor descr);
-    Descriptor descriptorFor(String ident);
-    int globalAddressOf(String ident);
-    int localAddressOf(String ident);
-    int size();
+import java.util.HashMap;
+import java.util.Map;
+
+public class SymbolTable {
+	private Map<String, Integer> addressMap = new HashMap<String, Integer>();
+	private Map<String, Descriptor> descriptorMap = new HashMap<String,Descriptor>();
+	private int currentAddress = 0;
+	private SymbolTable parentTable;
+	
+	public SymbolTable(){
+		this.parentTable = null;
+	}
+	
+	public SymbolTable(SymbolTable table){
+		this.parentTable = table;
+	}
+	
+	public static SymbolTable createSymbolTable(){
+		return new SymbolTable();
+	}
+	
+
+	
+	public void declare(String ident, Descriptor descr) {
+		descriptorMap.put(ident, descr);
+		addressMap.put(ident, currentAddress);
+		currentAddress += descr.size();
+	}
+	
+//	public void undeclare(String ident){
+//		descriptorMap.remove(ident);
+//		addressMap.remove(ident);
+//		
+//	}
+
+
+	public Descriptor descriptorFor(String ident) {
+		Descriptor d = descriptorMap.get(ident);
+		if(d == null && parentTable != null){
+			return parentTable.descriptorFor(ident);
+		}
+		return d;
+	}
+
+	
+
+	
+	public int addressOf(String ident) {
+		
+		if(addressMap.containsKey(ident)){
+			return addressMap.get(ident);
+		}
+		if(parentTable != null){
+			return parentTable.addressOf(ident);
+		}
+		return -1;
+		
+		
+	}
+
+	
+	public int size() {
+		return currentAddress;
+	}
+	
+	public static void main(String args[]){
+		System.out.println(SymbolTable.class.getClassLoader());
+		System.out.println(SymbolTable.class.getClassLoader());
+	}
+
 }
---
+++ src/haw/ai/ci/SymbolTableTest.java
@@ -0,0 +1,74 @@
+package haw.ai.ci;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+
+public class SymbolTableTest {
+	
+	
+	private class TestDescriptor implements Descriptor {
+		private int size;
+		
+		public TestDescriptor(int size){
+			this.size = size;
+		}
+		
+		public int size(){
+			return this.size;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getOuterType().hashCode();
+			result = prime * result + size;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			TestDescriptor other = (TestDescriptor) obj;
+			if (!getOuterType().equals(other.getOuterType()))
+				return false;
+			if (size != other.size)
+				return false;
+			return true;
+		}
+
+		private SymbolTableTest getOuterType() {
+			return SymbolTableTest.this;
+		}
+		
+		
+	}
+
+	@Test
+	public void test() {
+		Descriptor d = new TestDescriptor(5);
+		Descriptor x = new TestDescriptor(2);
+		
+		SymbolTable table = new SymbolTable();
+		assertEquals(0,table.size());
+		
+		table.declare("d", d);
+		assertEquals(5,table.size());
+		assertEquals(d,table.descriptorFor("d"));
+		assertEquals(0,table.addressOf("d"));
+		
+		table.declare("x", x);
+		assertEquals(7,table.size());
+		assertEquals(x,table.descriptorFor("x"));
+		assertEquals(5,table.addressOf("x"));
+	}
+
+}

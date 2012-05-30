8e38533e00db3dd826170cbcaafc4baf22f6110b
cyberaleks <aleksandr@nosov.de>
2012-05-30 23:48:02 +0200

    SimpleTypeDescriptor

---
+++ src/haw/ai/ci/descriptor/SimpleTypeDescriptor.java
@@ -0,0 +1,18 @@
+package haw.ai.ci.descriptor;
+
+public class SimpleTypeDescriptor implements Descriptor{
+	
+	final public static String STRING="string";
+	final public static String BOOLEAN="boolean";
+	final public static String INTEGER="integer";
+	private String type;
+	
+	public SimpleTypeDescriptor(String type) {
+		this.type=type;
+	}
+	
+	@Override
+	public int size() {
+		return 1;
+	}
+}

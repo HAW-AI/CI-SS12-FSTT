f6dea5a06c2829f3c713310870ffc192160e848f
philippgille <philipp.gille@haw-hamburg.de>
2012-05-31 13:58:40 +0200

    assign test erfolgreich

--- src/haw/ai/ci/CompileTest.java
+++ src/haw/ai/ci/CompileTest.java
@@ -1,5 +1,7 @@
 package haw.ai.ci;
 
+import haw.ai.ci.descriptor.SimpleTypeDescriptor;
+import haw.ai.ci.descriptor.SimpleTypeDescriptor.Type;
 import haw.ai.ci.node.AssignmentNode;
 import haw.ai.ci.node.IdentNode;
 import haw.ai.ci.node.IntNode;
@@ -8,9 +10,12 @@ import org.junit.Test;
 
 
 public class CompileTest {
-
+    
 	@Test
 	public void testAssign(){
 		AssignmentNode an = new AssignmentNode(new IdentNode("i"), new IntNode(7));
+		SymbolTable st= new SymbolTable();
+		st.declare("i", new SimpleTypeDescriptor(Type.INTEGER));
+		an.compile(st);
 	}
 }

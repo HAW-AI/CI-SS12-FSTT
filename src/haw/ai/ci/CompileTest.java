ebe290d6fe1115bd45b79b865978dd2a4736aa8a
philippgille <philipp.gille@haw-hamburg.de>
2012-05-31 14:53:56 +0200

    added binop compile and test

--- src/haw/ai/ci/CompileTest.java
+++ src/haw/ai/ci/CompileTest.java
@@ -3,6 +3,7 @@ package haw.ai.ci;
 import haw.ai.ci.descriptor.SimpleTypeDescriptor;
 import haw.ai.ci.descriptor.SimpleTypeDescriptor.Type;
 import haw.ai.ci.node.AssignmentNode;
+import haw.ai.ci.node.BinOpNode;
 import haw.ai.ci.node.IdentNode;
 import haw.ai.ci.node.IntNode;
 
@@ -10,12 +11,26 @@ import org.junit.Test;
 
 
 public class CompileTest {
-    
+    private void log(String s){
+    	System.out.println(s);
+    }
 	@Test
 	public void testAssign(){
-		AssignmentNode an = new AssignmentNode(new IdentNode("i"), new IntNode(7));
+		log("--Assign--");
+		log("// a:=7");
 		SymbolTable st= new SymbolTable();
-		st.declare("i", new SimpleTypeDescriptor(Type.INTEGER));
+		AssignmentNode an = new AssignmentNode(new IdentNode("a"), new IntNode(7));
+		st.declare("a", new SimpleTypeDescriptor(Type.INTEGER));
 		an.compile(st);
+		log("--AssignBinOp--");
+		log("// a:=a * b");
+		st.declare("b", new SimpleTypeDescriptor(Type.INTEGER));
+		AssignmentNode an2 = new AssignmentNode(new IdentNode("a"), new BinOpNode(BinOpNode.BinOp.MUL_OP, new IdentNode("a"), new IdentNode("b")));
+		an2.compile(st);
+		log("// a:=b + 2");
+		AssignmentNode an3 = new AssignmentNode(new IdentNode("a"), new BinOpNode(BinOpNode.BinOp.PLUS_OP, new IdentNode("b"), new IntNode(2)));
+		an3.compile(st);
 	}
+	
+	
 }

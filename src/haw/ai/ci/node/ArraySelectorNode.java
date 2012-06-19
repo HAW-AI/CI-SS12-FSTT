3abd7e25e6eb8dd2d63b6c3e47f5386129ab2d24
Felix Schmidt <felix.schmidt@haw-hamburg.de>
2012-06-19 18:05:28 +0200

    array assignment should copy now

--- src/haw/ai/ci/node/ArraySelectorNode.java
+++ src/haw/ai/ci/node/ArraySelectorNode.java
@@ -1,179 +1,88 @@
 package haw.ai.ci.node;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
-import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
-
-import cip.descriptor.ArrayDescr;
-import haw.ai.ci.CompilerException;
 import haw.ai.ci.SymbolTable;
 import haw.ai.ci.descriptor.ArrayDescriptor;
 import haw.ai.ci.descriptor.Descriptor;
 
 public class ArraySelectorNode extends SelectorNode {
-    private static final long serialVersionUID = 1L;
-    
-    private final AbstractNode subject;
-    private final AbstractNode selector;
+	private static final long serialVersionUID = 1L;
 
-    public ArraySelectorNode(IdentNode subject, AbstractNode selector) {
-        this((AbstractNode)subject, selector);
-    }
-    
-    public ArraySelectorNode(SelectorNode subject, AbstractNode selector) {
-        this((AbstractNode)subject, selector);
-    }
-    
-    private ArraySelectorNode(AbstractNode subject, AbstractNode selector) {
-        this.subject = subject;
-        this.selector = selector;
-    }
+	private final AbstractNode subject;
+	private final AbstractNode selector;
 
-    @Override
-    protected String toString(int indent) {
-        return toString(indent, "ArraySelectorNode\n")
-                + subject.toString(indent + 1) + "\n"
-                + selector.toString(indent + 1);
-    }
+	public ArraySelectorNode(IdentNode subject, AbstractNode selector) {
+		this((AbstractNode) subject, selector);
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result
-                + ((selector == null) ? 0 : selector.hashCode());
-        result = prime * result + ((subject == null) ? 0 : subject.hashCode());
-        return result;
-    }
+	public ArraySelectorNode(SelectorNode subject, AbstractNode selector) {
+		this((AbstractNode) subject, selector);
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        ArraySelectorNode other = (ArraySelectorNode) obj;
-        if (selector == null) {
-            if (other.selector != null)
-                return false;
-        } else if (!selector.equals(other.selector))
-            return false;
-        if (subject == null) {
-            if (other.subject != null)
-                return false;
-        } else if (!subject.equals(other.subject))
-            return false;
-        return true;
-    }
-    
-    public int address() {
-        return 0;
-    }
-    
-    /**
-     * 
-     * @param symbolTable
-     * @return array with 2 elements (size and address of the resolved selector)
-     */
-    public int[] sizeAndAddress(SymbolTable symbolTable) {
-        return sizeAndAddress(symbolTable, new LinkedList<Integer>());
-    }
-    
-    public int[] sizeAndAddress(SymbolTable symbolTable, List<Integer> idxs) {
-        if (selector instanceof IdentNode) {
-            throw new CompilerException("We cannot handle idents as indices :(");
-        }
-        idxs.add((Integer)selector.getVal());
-        
-        if (subject instanceof IdentNode) {
-            String name = ((IdentNode)subject).getIdentName();
-            ArrayDescriptor d = (ArrayDescriptor)symbolTable.descriptorFor(name);
-            
-            int addr = symbolTable.addressOf(name);
-            idxs = new ArrayList<Integer>(idxs);
-            
-            for (int i = idxs.size()-1; i > 0; --i) {
-                // doppeltgemoppelt?!
-                if (!(d instanceof ArrayDescriptor)) {
-                    throw new CompilerException("We can only handle ArrayDescriptors :(");
-                }
-                
-                addr += d.size() * idxs.get(i);
-                d = (ArrayDescriptor)d.basetype();
-            }
-            
-            int size = d.size();
-            
-            int[] res = { size, addr };
-            return res;
-        } else {
-            if (subject instanceof ArraySelectorNode) {
-                return ((ArraySelectorNode)subject).sizeAndAddress(symbolTable, idxs); 
-            } else {
-                throw new CompilerException("Subject is not an ArraySelectorNode but: " + subject);
-            }
-        }
-    }
+	private ArraySelectorNode(AbstractNode subject, AbstractNode selector) {
+		this.subject = subject;
+		this.selector = selector;
+	}
 
-    public Descriptor descriptor(SymbolTable symbolTable) {
-        return descriptor(symbolTable, new LinkedList<Integer>());
-    }
+	@Override
+	protected String toString(int indent) {
+		return toString(indent, "ArraySelectorNode\n") + subject.toString(indent + 1) + "\n" + selector.toString(indent + 1);
+	}
 
-    public Descriptor descriptor(SymbolTable symbolTable, List<Integer> idxs) {
-        if (selector instanceof IdentNode) {
-            throw new CompilerException("We cannot handle idents as indices :(");
-        }
-        idxs.add((Integer)selector.getVal());
-        
-        if (subject instanceof IdentNode) {
-            String name = ((IdentNode)subject).getIdentName();
-            ArrayDescriptor d = (ArrayDescriptor)symbolTable.descriptorFor(name);
-            
-            idxs = new ArrayList<Integer>(idxs);
-            
-            for (int i = idxs.size()-1; i > 0; --i) {
-                // doppeltgemoppelt?!
-                if (!(d instanceof ArrayDescriptor)) {
-                    throw new CompilerException("We can only handle ArrayDescriptors :(");
-                }
-                
-                d = (ArrayDescriptor)d.basetype();
-            }
-            
-            return d.basetype();
-        } else {
-            if (subject instanceof ArraySelectorNode) {
-                return ((ArraySelectorNode)subject).descriptor(symbolTable, idxs); 
-            } else {
-                throw new CompilerException("Subject is not an ArraySelectorNode but: " + subject);
-            }
-        }
-    }
-    
-    public Descriptor compile(SymbolTable table){
-    	ArrayDescriptor d;
-    	int typeSize;
-    	if(subject instanceof IdentNode){
-    		d = (ArrayDescriptor) table.descriptorFor(((IdentNode)subject).getIdentName());
-    		typeSize = d.basetype().size();
-    		subject.compile(table);
-    		selector.compile(table);
-    		write("PUSHI, " + typeSize);
-    		write("MUL");    		
-    		write("ADD");
-    	}else{
-    		d = (ArrayDescriptor)subject.compile(table);
-    		typeSize = d.basetype().size();
-    		selector.compile(table);
-    		write("PUSHI, "+ typeSize);
-    		write("MUL");
-    		write("ADD");
-    	}
-    	return d.basetype();
-    }
-    
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((selector == null) ? 0 : selector.hashCode());
+		result = prime * result + ((subject == null) ? 0 : subject.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ArraySelectorNode other = (ArraySelectorNode) obj;
+		if (selector == null) {
+			if (other.selector != null)
+				return false;
+		} else if (!selector.equals(other.selector))
+			return false;
+		if (subject == null) {
+			if (other.subject != null)
+				return false;
+		} else if (!subject.equals(other.subject))
+			return false;
+		return true;
+	}
+
+	public int address() {
+		return 0;
+	}
+
+	public Descriptor compile(SymbolTable table) {
+		ArrayDescriptor d;
+		int typeSize;
+		if (subject instanceof IdentNode) {
+			d = (ArrayDescriptor) table.descriptorFor(((IdentNode) subject).getIdentName());
+			typeSize = d.basetype().size();
+			subject.compile(table);
+			selector.compile(table);
+			write("PUSHI, " + typeSize);
+			write("MUL");
+			write("ADD");
+		} else {
+			d = (ArrayDescriptor) subject.compile(table);
+			typeSize = d.basetype().size();
+			selector.compile(table);
+			write("PUSHI, " + typeSize);
+			write("MUL");
+			write("ADD");
+		}
+		return d.basetype();
+	}
+
 }
--- src/haw/ai/ci/node/ContentNode.java
+++ src/haw/ai/ci/node/ContentNode.java
@@ -7,77 +7,81 @@ import haw.ai.ci.descriptor.IntConstDescriptor;
 import haw.ai.ci.descriptor.RecordDescriptor;
 
 public class ContentNode extends AbstractNode {
-    
-    private static final long serialVersionUID = 1L;
-    
-    private AbstractNode subject;
-    
-    public ContentNode(IdentNode ident) {
-        this((AbstractNode)ident);
-    }
-    
-    public ContentNode(SelectorNode selector) {
-        this((AbstractNode)selector);
-    }
-    
-    private ContentNode(AbstractNode subject) {
-        this.subject = subject;
-    }
 
-    @Override
-    protected String toString(int indent) {
-        return toString(indent, "ContentNode(" + subject + ")");
-    }
-	public AbstractNode getSubject(){
+	private static final long serialVersionUID = 1L;
+
+	private AbstractNode subject;
+
+	public ContentNode(IdentNode ident) {
+		this((AbstractNode) ident);
+	}
+
+	public ContentNode(SelectorNode selector) {
+		this((AbstractNode) selector);
+	}
+
+	private ContentNode(AbstractNode subject) {
+		this.subject = subject;
+	}
+
+	@Override
+	protected String toString(int indent) {
+		return toString(indent, "ContentNode(" + subject + ")");
+	}
+
+	public AbstractNode getSubject() {
 		return subject;
 	}
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((subject == null) ? 0 : subject.hashCode());
-        return result;
-    }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        ContentNode other = (ContentNode) obj;
-        if (subject == null) {
-            if (other.subject != null)
-                return false;
-        } else if (!subject.equals(other.subject))
-            return false;
-        return true;
-    }
-    
-    @Override
-    public Descriptor compile(SymbolTable symbolTable) {
-    	Descriptor d = null;
-    	
-    	if(subject instanceof IdentNode) {
-    		d = symbolTable.descriptorFor(((IdentNode)subject).getIdentName());
-    	} else if (subject instanceof ArraySelectorNode) {
-    	    d = ((ArraySelectorNode)subject).descriptor(symbolTable);
-    	}
-    	
-        if(d instanceof IntConstDescriptor){
-        	write("PUSHI, "+((IntConstDescriptor)d).value());
-        }else if(d instanceof RecordDescriptor || d instanceof ArrayDescriptor){
-            subject.compile(symbolTable);
-            write("CONT, " + d.size());
-        	return d;
-        }else{
-        	subject.compile(symbolTable);
-        	write("CONT, 1");
-        }
-        
-        return null;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((subject == null) ? 0 : subject.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ContentNode other = (ContentNode) obj;
+		if (subject == null) {
+			if (other.subject != null)
+				return false;
+		} else if (!subject.equals(other.subject))
+			return false;
+		return true;
+	}
+
+	@Override
+	public Descriptor compile(SymbolTable symbolTable) {
+		Descriptor d = null;
+
+		if (subject instanceof IdentNode) {
+			d = symbolTable.descriptorFor(((IdentNode) subject).getIdentName());
+		}
+
+		if (d instanceof IntConstDescriptor) {
+			write("PUSHI, " + ((IntConstDescriptor) d).value());
+		} else if (subject instanceof RecordSelectorNode || subject instanceof ArraySelectorNode || d instanceof RecordDescriptor || d instanceof ArrayDescriptor) {
+			if (d == null) {
+				d = subject.compile(symbolTable);
+			} else {
+				subject.compile(symbolTable);
+			}
+			write("CONT, " + d.size());
+			return d;
+		} else {
+			subject.compile(symbolTable);
+			write("CONT, 1");
+		}
+
+		return null;
+	}
 
 }
